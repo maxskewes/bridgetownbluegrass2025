3a7f9adde4d3c8982e4634630ec74b1128bd31b8
Maxwell Countryman Skewes <maxskewes@gmail.com>
2024-05-22 17:06:55 -0700

    Restyled page/content limiters, base padding, etc

--- app/artist-apply/page.tsx
+++ app/artist-apply/page.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import PageContainer from '../components/PageContainer';
 import ArtistForm from './ArtistForm';
 import PageTextBox from '../components/PageTextBox';
+import CenterTextBox from '../components/CenterTextBox';
 
 const ArtistApply = () => {
   return (
     <PageContainer heading='Artist Apply'>
+      <CenterTextBox>
+        We are not currently accepting applications for our forth-coming 2025
+        festival. <br /> Please see below for application criteria and check back in with
+        in the new year!
+      </CenterTextBox>
       <PageTextBox>
-        Now accepting applications for 2024! The Bridgetown Bluegrass & Folk
-        Festival seeks hard-working artists that are actively creating new and
-        original music, and demonstrate a commitment to bringing that music to
-        the public through consistent live performance. In this submission form,
-        tell us about yourself/your band, send us links to your music, and show
-        us what you<span>&#39;</span>ve been up to. Submissions for Bridgetown
-        2024 close on March 1, 2024. All artists will be offered at least one 30
-        minute set, wristbands for the day of the fest, access to Bridgetown
-        Green Room and Hospitality Lounges, and a +1 per band member.
+        The Bridgetown Bluegrass & Folk Festival seeks hard-working artists that
+        are actively creating new and original music, and demonstrate a
+        commitment to bringing that music to the public through consistent live
+        performance. In this submission form, tell us about yourself/your band,
+        send us links to your music, and show us what you<span>&#39;</span>ve
+        been up to. Submissions for Bridgetown 2024 close on March 1, 2024. All
+        artists will be offered at least one 30 minute set, wristbands for the
+        day of the fest, access to Bridgetown Green Room and Hospitality
+        Lounges, and a +1 per band member.
       </PageTextBox>
       <PageTextBox>
         RADIUS CLAUSE: All artists confirmed for Bridgetown 2024 will be asked
